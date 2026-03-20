1ae4f7653c0aa2705b6f29e67e2e80169e6839a3
Sander Philipse <sander.philipse@elastic.co>
2026-03-20 19:43:01 +0100

    Fix types

--- x-pack/platform/plugins/shared/streams_app/public/components/significant_events_discovery/components/settings/tab.tsx
+++ x-pack/platform/plugins/shared/streams_app/public/components/significant_events_discovery/components/settings/tab.tsx
@@ -135,7 +135,7 @@ export function SettingsTab() {
   const showNoDefaultCallout = !genAiConnectors.loading && !hasDefaultConnector && anyUsesDefault;
   const defaultConnectorName =
     hasDefaultConnector && anyUsesDefault
-      ? genAiConnectors.connectors?.find((c) => c.id === genAiConnectors.defaultConnector)?.name
+      ? genAiConnectors.connectors?.find((c) => c.connectorId === genAiConnectors.defaultConnector)?.name
       : undefined;
 
   return (
